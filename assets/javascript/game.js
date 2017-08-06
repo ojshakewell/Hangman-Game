741db276d94d84915c6f01fc77843d86005db811
Brian Russell <brianrobertrussell@gmail.com>
2017-08-05 22:26:35 -0600

    working version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,17 @@
 //game.js for Sesame Street Hangman-Game
 //Brian Russell
 //2017
-
 //Variables
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var muppet = ["bert","ernie","elmo","grover","abby","snuffleupagus","oscar", "bigbird"];
+var muppet = ["bert", "ernie", "elmo", "grover", "abby"];
 
 var blank;
 var blankMuppet;
 var chosenMuppet;
 var correctGuess;
-var	guessesLeft;
-var	guessesMade;
+var guessesLeft;
+var guessesMade;
 var lettersGuessed;
 var wins = 0;
 
@@ -20,130 +19,99 @@ var wins = 0;
 resetVariables();
 
 // Get user input
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 
-	// resets correct guess toggle 
-	var correctGuess = false;
+    // resets correct guess 
+    var correctGuess = false;
 
-	// stores user input from keyboard, lowercase letter only
-	var keyPressed = String.fromCharCode(event.keyCode).toLowerCase();
-	console.log ("input: " + keyPressed);
+    
 
-	//loop through array to see if the letter has alreay been guessed
-	for (var i = 0; i < chosenMuppet.length; i++) {
+    // stores user input from keyboard, lowercase letter only
+    var keyPressed = String.fromCharCode(event.keyCode).toLowerCase();
+    console.log("input: " + keyPressed);
 
-		if (chosenMuppet[i] === keyPressed){
-			//enter keyboard input into blank array
-			blankMuppet[i] = keyPressed;
+    //loop through array to see if the letter has alreay been guessed
+    for (var i = 0; i < chosenMuppet.length; i++) {
 
-			correctGuess = true;
-		}
-	};
+        if (chosenMuppet[i] === keyPressed) {
+            //enter keyboard input into blank array
+            blankMuppet[i] = keyPressed;
 
-	//changes array into sting for display purposes
-	var lettersGuessedDisplay = lettersGuessed.join(" ");
+            correctGuess = true;
+        }
+    };
+
+    //changes array into sting for display purposes
+    var lettersGuessedDisplay = lettersGuessed.join(" ");
 
 
-	//ignores the input if the same letter is presse more than once
-	var duplicateLetter = lettersGuessedDisplay.includes(keyPressed)
-	console.log("dup:"+ duplicateLetter);
-	if (duplicateLetter === false){
-		lettersGuessed.push(keyPressed);
+    //ignores the input if the same letter is presse more than once
+    var duplicateLetter = lettersGuessedDisplay.includes(keyPressed)
+    console.log("dup:" + duplicateLetter);
+    if (duplicateLetter === false) {
+        lettersGuessed.push(keyPressed);
 
-		//reduce guesses only if the letter is not in the word
-		if (correctGuess === false){
-			guessesLeft--;	
-		}
-	}
+        //reduce guesses only if the letter is not in the word
+        if (correctGuess === false) {
+            guessesLeft--;
+        }
+    }
 
-	// reset win or loss
-	var unsolved = blankMuppet.includes("_");
-	
-	if (unsolved === false){
-		wins++;
-		resetVariables();
-		// switch (chosenMuppet){
+    // reset win or loss
+    var unsolved = blankMuppet.includes("_");
 
-		// 	case "bert":
-		// 		console.log(chosenMuppet+'wins')
-		// 	break;
+    var muppetPicture = "";
 
-		// 	case "ernie":
-		// 		console.log(chosenMuppet+'wins')
-		// 	break;
+    if (unsolved === false) {
+        wins++;
+        resetVariables();
+    } else if (guessesLeft === 0) {
+        wins--;
+        resetVariables();
+        //play loser video
+    };
+    //update current word on html
+    var blankMuppetDisplay = blankMuppet.join(" ");
 
-		// 	case "elmo":
-				
-		// 	break;
+    document.querySelector(".blanks").innerHTML = blankMuppetDisplay;
 
-		// 	case "grover":
-		// 		console.log(chosenMuppet+'wins')
-		// 	break;
+    //update number of wins on html
+    document.querySelector(".wins").innerHTML = wins;
 
-		// 	case "abby":
-		// 		console.log(chosenMuppet+'wins')
-		// 	break;
+    //Show remaining guesses
+    document.querySelector(".remaining").innerHTML = guessesLeft;
 
-		// 	case "snuffleupagus":
-		// 		console.log(chosenMuppet+'wins')			
-		// 	break;
+    //Update the Letters Guessed <h3> in HTML
+    lettersGuessedDisplay = lettersGuessed.join(" ");
 
-		// 	case "oscar":
-		// 		console.log(chosenMuppet+'wins')
-		// 	break;
+    document.querySelector(".guess").innerHTML = lettersGuessedDisplay;
+}; //end document.onkeyup
 
-		// 	case "bigbird":
-		// 		console.log(chosenMuppet+'wins')
-		// 	break; 
-		}
-		//play winning video
-	} else if (guessesLeft === 0){ 
-		wins--;
-		resetVariables();
-		//play loser video
-	};
-	//update current word on html
-	var blankMuppetDisplay = blankMuppet.join(" ");
+function resetVariables() {
 
-	document.querySelector(".blanks").innerHTML = blankMuppetDisplay;
+    blankMuppet = [];
+    guessesLeft = 10;
+    guessesMade = [];
+    lettersGuessed = [];
 
-	//update number of wins on html
-	document.querySelector(".wins").innerHTML = wins;
+    //chose muppet name from Array
+    chosenMuppet = muppet[Math.floor(Math.random() * muppet.length)];
+    console.log(chosenMuppet);
 
-	//Show remaining guesses
-	document.querySelector(".remaining").innerHTML = guessesLeft;
+    //Split muppen Name into letters
+    var muppetChars = chosenMuppet.split('');
 
-	//Update the Letters Guessed <h3> in HTML
-	lettersGuessedDisplay = lettersGuessed.join(" ");
+    //create an array of blank spaces basetd on the length of the name
+    for (var i = 0; i < chosenMuppet.length; i++) {
 
-	document.querySelector(".guess").innerHTML = lettersGuessedDisplay;
-};//end document.onkeyup
+        blankMuppet.push("_");
 
-function resetVariables(){
+        //joins array of blank spaces into a single word
+        blank = blankMuppet.join(" ");
 
-	blankMuppet = [];
-	guessesLeft = 10;
-	guessesMade = [];
-	lettersGuessed = [];
+        //update html
+        var html = '<h3>' + blank + '</h3>'
 
-	//chose muppet name from Array
-	chosenMuppet = muppet[Math.floor(Math.random()*muppet.length)];
-		console.log(chosenMuppet);
-
-	//Split muppen Name into letters
-	var muppetChars = chosenMuppet.split('');
-
-	//create an array of blank spaces basetd on the length of the name
-	for (var i = 0; i < chosenMuppet.length; i++) {
-
-		blankMuppet.push("_");
-
-	//joins array of blank spaces into a single word
-	blank = blankMuppet.join(" ");
-
-	//update html
-	var html='<h3>' + blank + '</h3>'
-
-	document.querySelector(".blanks").innerHTML = html;
-	}		
-};//end resetVariables()
+        document.querySelector(".blanks").innerHTML = html;
+    }
+}; //end resetVariables()
